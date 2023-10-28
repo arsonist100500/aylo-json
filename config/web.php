9d59d02569d4ca44263b377871cd65bdf68f3d27
arsonist100500 <orlov.viktor@gmail.com>
2023-10-28 17:51:03 +0300

    update config: enable pretty urls, set cookieValidationKey

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'hQmixLPUWXXEnRHP1owIeJlXijH7QvHP',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -42,14 +42,12 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
